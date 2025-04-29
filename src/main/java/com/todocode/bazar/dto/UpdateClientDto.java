9cbfd919f48693bf6c55f3cea75180dcd1bcac8a
jcaparr <juancaparr0s@hotmail.com>
2025-04-29 14:20:43 -0300

    add(SaleController): add new controller.

--- src/main/java/com/todocode/bazar/dto/UpdateClientDto.java
+++ src/main/java/com/todocode/bazar/dto/UpdateClientDto.java
@@ -1,9 +1,7 @@
 package com.todocode.bazar.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
